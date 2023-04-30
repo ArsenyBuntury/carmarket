1f7b15ba1c3875439f2c735c6d16f340270a179b
Arseny <yeahwheel@gmail.com>
2023-04-30 21:37:16 +0300

    minor changes in dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bin/rails server -b 0.0.0.0 | bin/rails db:create RAILS_ENV=development | bin/rails db:migrate RAILS_ENV=development
+bin/rails db:create db:migrate  db:seed RAILS_ENV=development && bin/rails server -b 0.0.0.0 
 
